bd183ade5be9b6e612c7aa828f16d607e11fe0f7
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-11-22 15:22:07 +0100

    Removed rediculous redundant statemet

--- src/main/java/com/switchfully/springdi/springdi04attempt2/config/AppConfig.java
+++ src/main/java/com/switchfully/springdi/springdi04attempt2/config/AppConfig.java
@@ -2,19 +2,12 @@ package com.switchfully.springdi.springdi04attempt2.config;
 
 import com.switchfully.springdi.springdi04attempt2.TaxCalculator;
 import com.switchfully.springdi.springdi04attempt2.taxCalculation.AmericanTaxCalculation;
-import com.switchfully.springdi.springdi04attempt2.taxCalculation.BelgianTaxCalculation;
 import com.switchfully.springdi.springdi04attempt2.taxCalculation.TaxCalculation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    @Primary
-    public TaxCalculation belgianTaxCalculation() {
-        return new BelgianTaxCalculation();
-    }
 
     @Bean("America")
     public TaxCalculation americanTaxCalculation() {
